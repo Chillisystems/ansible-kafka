180355147a3d9e0c264f7bd11bd73807382c1416
Adam Hamsik <adam.hamsik@chillisys.com>
2017-05-10 13:45:18 +0200

    Add some more tests.

--- tests/test_default.py
+++ tests/test_default.py
@@ -11,3 +11,22 @@ def test_packages(Package, name, version):
     pkg = Package(name)
     assert pkg.is_installed
     assert pkg.version.startswith(version)
+
+@pytest.mark.parametrize('service_name', [
+    ('zookeeper'),
+    ('kafka'),
+])
+def test_kafka_service_enabled(Service, service_name):
+    s = Service(service_name)
+
+    assert s.is_running
+    assert s.is_enabled
+
+@pytest.mark.parametrize('service_url', [
+    ('tcp://:::2181'),
+    ('tcp://:::9092'),
+])
+def test_kafka_is_running(Socket, service_url):
+    s = Socket(service_url)
+
+    assert s.is_listening
